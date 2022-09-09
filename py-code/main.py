cab52bd722d54d29f658fd42a0b129003a4ca007
Olga Terekhova <olga.terekhova@gmail.com>
2022-09-09 18:22:24 -0400

    Login into a page, grab time and status

--- py-code/main.py
+++ py-code/main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.common.by import By
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_credentials():
+    f = open('cred.config')
+    data = json.load(f)
+    #print(data)
+    cred_dict = []
+    for i in data['credentials']:
+        #print(i)
+        cred_dict.append(i)
+    f.close()
+    return cred_dict
+
+def check_status_person(credentials):
+    print(credentials['login'])
+    print(credentials['password'])
+    service = Service('C:/Users/terem/Downloads/chromedriver_win32/chromedriver.exe')
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+    driver.get('https://cst-ssc.apps.cic.gc.ca/en/login')
+    time.sleep(5)  # Let the user actually see something!
+    login_element = driver.find_element('id', 'uci-input')
+    login_element.send_keys(credentials['login'])
+    time.sleep(1)
+    pwd_element = driver.find_element('id', 'password-input')
+    pwd_element.send_keys(credentials['password'])
+    submit = driver.find_element('id', 'sign-in-submit-btn')
+    submit.click()
+    time.sleep(5)
+    app_history = driver.find_element("xpath", "//h2[text()='Application history']")
+    grandparent = app_history.find_element("xpath","../..")
+    #print(grandparent.tag_name)
+    times = grandparent.find_elements("tag name", "time")
+    #print(times[0].tag_name)
+    #print(len(times))
+    for t in times:
+        if t.value_of_css_property("white-space") == 'nowrap':
+            print(t.text)
+            print(t.get_attribute('datetime'))
+            grandparent_time = t.find_element("xpath", "../..")
+            status = grandparent_time.find_element("tag name", "h3")
+            print(status.text)
+    time.sleep(5)  # Let the user actually see something!
+
+    # get status
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    driver.quit()
 
+def check_status_all():
+    #for i in get_credentials():
+    #    check_status_person(i)
+    check_status_person(get_credentials()[0])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+check_status_all()
